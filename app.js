c445ebe12f014ddbffee7a27603ac2e463dc74dd
onlyarif <shahidul65869704@gmail.com>
2022-03-11 20:04:27 -0800

    solution

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 let computerGuesss;
-let userguess =[];
+let userguess = [];
 let userGuessNumber = document.getElementById("textOutput");
 let userNumerUpdate = document.getElementById("inputBox");
-let audio =new Audio("./song/song2.wav")
-
-
+let audio = new Audio("./song/song2.wav");
 
 const init = () => {
   computerGuesss = parseInt(Math.random() * 100);
@@ -21,79 +19,65 @@ const stratgame = () => {
 };
 
 const easyMode = () => {
-    audio.play();
+  audio.play();
   maxgesss = 10;
   stratgame();
 };
 const hardMode = () => {
-    audio.play();
+  audio.play();
   maxgesss = 5;
   stratgame();
 };
 
-// reload the page 
+// reload the page
 
-const newGameBegin=()=>{
-    
-    audio.play();
+const newGameBegin = () => {
+  audio.play();
   window.location.reload();
-}
+};
 
 //strat new game button
-const stratNewgame=()=>{
-    document.getElementById("newGameButon").style.display = "inline";
-    userNumerUpdate.setAttribute("disabled",true)
-}
-
-
+const stratNewgame = () => {
+  document.getElementById("newGameButon").style.display = "inline";
+  userNumerUpdate.setAttribute("disabled", true);
+};
 
 // main lolic of our app
 const compareGess = () => {
-  const userNumber =document.getElementById("inputBox").value;
+  const userNumber = document.getElementById("inputBox").value;
   audio.play();
 
   userguess = [...userguess, userNumber];
   document.getElementById("guess").innerHTML = userguess;
   // check the number low or high
 
-  if(userguess.length<maxgesss){
+  if (userguess.length < maxgesss) {
     if (userNumber > computerGuesss) {
-        userGuessNumber.innerText = "Your Guess is very High 😲";
-        userNumerUpdate.value = "";
-      }
-       else if (userNumber < computerGuesss) {
-        userGuessNumber.innerText = "Your Guess is very low 😌 ";
-        userNumerUpdate.value = "";
-      }
-       else {
-        userGuessNumber.innerText = "It's correct 😃 ";
-        userNumerUpdate.value = "";
-        }
-
+      userGuessNumber.innerText = "Your Guess is very High 😲";
+      userNumerUpdate.value = "";
+    } else if (userNumber < computerGuesss) {
+      userGuessNumber.innerText = "Your Guess is very low 😌 ";
+      userNumerUpdate.value = "";
+    } else {
+      userGuessNumber.innerText = "It's correct 😃 ";
+      userNumerUpdate.value = "";
+      stratNewgame();
     }
-
-else{
+  } else {
     if (userNumber > computerGuesss) {
-        userGuessNumber.innerText = `You Loose !! Courect Answer Was ${computerGuesss}`;
-        userNumerUpdate.value = "";
-        stratNewgame();
-      }
-       else if (userNumber < computerGuesss) {
-        userGuessNumber.innerText = `You Loose !! Courect Answer Was ${computerGuesss}`;
-        userNumerUpdate.value = "";
-        stratNewgame();
-      }
-       else {
-        userGuessNumber.innerText = "It's correct 😃 ";
-        userNumerUpdate.value = "";
-        stratNewgame();
-        }
-}
+      userGuessNumber.innerText = `You Loose !! Courect Answer Was ${computerGuesss}`;
+      userNumerUpdate.value = "";
+      stratNewgame();
+    } else if (userNumber < computerGuesss) {
+      userGuessNumber.innerText = `You Loose !! Courect Answer Was ${computerGuesss}`;
+      userNumerUpdate.value = "";
+      stratNewgame();
+    } else {
+      userGuessNumber.innerText = "It's correct 😃 ";
+      userNumerUpdate.value = "";
+      stratNewgame();
+    }
+  }
 
-
-document.getElementById('attempts').innerText = userguess.length;
-    
-
-   
-  
-}
+  document.getElementById("attempts").innerText = userguess.length;
+};
